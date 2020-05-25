0739a94c3c543dccb9966a279eba9f0a8c33461d
grsb99 <grsb99@gmail.com>
2020-05-25 02:57:54 +0200

    frontend design

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 import ListItems from "./components/listOfTodos";
 
+export function Rtif({boolean, ...props}) {
+    const { children } = props;
+    if (boolean)
+        return (
+            {...children}
+        );
+    return null;
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -17,6 +26,7 @@ class App extends React.Component {
     this.deleteTodo = this.deleteTodo.bind(this);
     this.updateTodo = this.updateTodo.bind(this);
   }
+
 
   handleInput(e) {
       this.setState({
@@ -69,14 +79,61 @@ class App extends React.Component {
   render() {
     return (
         <div className="App">
-          <header>
-              <form id="todoForm" onSubmit={this.addTodo}>
-                  <input type="text" value={this.state.currentItem.text} onChange={this.handleInput}/>
-                  <button type="submit">Add</button>
-              </form>
+            <div className="createdHolder">
+                <img src="avatar.png" alt=""/>
+                <p id="created">Created by Geresdi Bence</p>
+            </div>
+            <img id="ball1" src="Ellipse 22.png" alt=""/>
+            <img id="ball2" src="Ellipse 21.png" alt=""/>
+            <img id="ball3" src="Ellipse 30.png" alt=""/>
+            <img id="ball4" src="Ellipse 23.png" alt=""/>
+            <img id="ball5" src="Rectangle 36.png" alt=""/>
+            <img id="ball6" src="Group 20.png" alt=""/>
 
-          </header>
-            <ListItems items = {this.state.items} deleteItem = {this.deleteTodo} updateItem = {this.updateTodo}></ListItems>
+
+
+
+
+            <img className="points" src="Group 12.png" alt=""/>
+            <div className="leftMain">
+              <div className="titleHolder">
+                  <h1>My Todo App</h1>
+                  <p>Welcome back to the app, we missed You!</p>
+              </div>
+              <div className="imageHolder">
+                  <img className="mainImg" src="Group 4.png" alt=""/>
+              </div>
+
+          </div>
+
+            <div className="rightMain">
+                <div className="textHolder">
+                    <h1>Here is your Todo List</h1>
+                    <p>Add task with the plus button and complete them. Easily follow your daily tasks and be successful! Click on the todo text to edit!</p>
+                </div>
+                <div className="inputHolder">
+                    <form id="todoForm" onSubmit={this.addTodo}>
+                        <input placeholder="Type here your todo..." type="text" value={this.state.currentItem.text} onChange={this.handleInput}/>
+                        <button type="submit">+</button>
+                    </form>
+                </div>
+                <div className="dayHolder">
+                    <h3>Todos</h3>
+                    <hr/>
+                </div>
+                <div className="todoHolder">
+                    <Rtif boolean={this.state.items.length == 0}>
+                        <div>
+                            <img  src="undraw_completed_ngx6.png" alt=""/>
+                            <h2>Celebrate, you're all done.</h2>
+                            <p>Have a good day!</p>
+                        </div>
+                    </Rtif>
+
+                    <ListItems items = {this.state.items} deleteItem = {this.deleteTodo} updateItem = {this.updateTodo}></ListItems>
+                </div>
+
+            </div>
         </div>
     )
   }
--- todo-list/src/components/listOfTodos.js
+++ todo-list/src/components/listOfTodos.js
@@ -1,17 +1,32 @@
 import React from "react";
-import './listOfTodos.css'
+import './listOfTodos.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
  function ListItems(props) {
     const items = props.items;
+
     const listItems = items.map(item => {
+
         return <div className="list" key={item.key}>
-            <p>
-                <input type="text"
-                      id={item.key}
-                      value={item.text}
-                       onChange={(e) => props.updateItem(e.target.value, item.key)}
-                />
-            </p>
-            <span><p onClick={() => props.deleteItem(item.key)}>delete</p></span>
+            <div className="firstTag">
+                <label htmlFor={item.key}>
+                    <input id={item.key} type="checkbox"/>
+                    <i></i>
+                    <p>
+                        <input type="text"
+                               id={item.key}
+                               value={item.text}
+                               onChange={(e ) => props.updateItem(e.target.value, item.key)}
+                        />
+                    </p>
+                </label>
+
+
+
+            </div>
+
+            <span><p onClick={() => props.deleteItem(item.key)}><FontAwesomeIcon icon={faTrash}/></p></span>
         </div>
     })
     return(
